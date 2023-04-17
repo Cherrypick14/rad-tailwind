aecb1205ae4fb9b6443ec2354e895001460b2fa5
Cherrypick14 <cherrypicklet@gmail.com>
2023-04-17 06:22:18 +0300

    add our header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from 'react';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      <h1>I am limitless</h1>
+      <Header />
     </div>
   );
 }
