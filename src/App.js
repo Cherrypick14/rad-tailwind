0367ebcb47b03b5cd8a13e2abb8fae843d07ac72
Cherrypick14 <cherrypicklet@gmail.com>
2023-04-19 08:24:35 +0300

    add our navbar grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Header from './components/header/header';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='w-full min-h-[90vh] grid grid-cols-12'>
+        <Navbar />
+        <div>
+       {/* {leftbar} */}
+        {/* {rightbar} */}
+        </div>
+        
+      </div>
     </div>
   );
 }
