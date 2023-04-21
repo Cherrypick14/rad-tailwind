fbecfff5f71fe8270d5d2d8d230b1ffab6d6c2f1
Cherrypick14 <cherrypicklet@gmail.com>
2023-04-21 21:34:02 +0300

    add headings and paragraphs

--- src/components/leftbar/leftbar.jsx
+++ src/components/leftbar/leftbar.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Leftbar = () => {
   return (
-    <div>leftbar</div>
+    <div className='w-full flex flex-col min-h-[90vh] col-span-2 border-r border-cyan-700'> 
+        {/* {Top section} */}
+        <div className='w-full flex flex-col items-start justify-start pb-6 px-12 pt-12'>
+
+          <h1>My Cards</h1>
+          <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi voluptatem temporibus explicabo neque sed. Necessitatibus</p>
+          <div>
+            <h1></h1>
+            <h1></h1>
+            <h1></h1>
+
+          </div>
+        </div>
+        {/* {Bottom Section} */}
+    </div>
   )
 }
 
