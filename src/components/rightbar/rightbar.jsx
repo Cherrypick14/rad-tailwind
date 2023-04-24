02a617584d865af6d994323a91e9ccd2b3cb96aa
Cherrypick14 <cherrypicklet@gmail.com>
2023-04-24 15:06:10 +0300

    add right bar content

--- src/components/rightbar/rightbar.jsx
+++ src/components/rightbar/rightbar.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Rightbar = () => {
   return (
-    <div>rightbar</div>
+    <div className='w-full flex flex-col items-start justify-start pt-12 pb-6'>
+      {/* {Top Section} */}
+      <div className='md:flex lg:space-y-0 lg:space-x-4 px-12 items-center justify-center space-y-4'>
+        <div>
+        <span></span>
+        <span></span>
+        </div>
+        
+      </div>
+      {/* {Chart} */}
+      {/* {Bottom section} */}
+    </div>
   )
 }
 
